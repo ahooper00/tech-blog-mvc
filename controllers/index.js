e030811e1851155f0604ff0c77e99e3569eb8a7c
Amelia Hooper <ajhooper97@gmail.com>
2021-11-30 16:50:47 +1100

    Edited index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,21 +1,11 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes');
-// const homeRoutes = require('./homeRoutes');
+const homeRoute = require('./homeRoutes');
 
-// router.use('/', homeRoutes);
-router.get("/homepage", (req, res) => {
-  res.render("homepage");
+router.use('/', homeRoute);
+router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+    res.status(404).end();
 });
-
-router.get("/signup", (req, res) => {
-  res.render("signup");
-});
-
-router.get("/login", (req, res) => {
-  res.render("login");
-});
-
-router.use("/api", apiRoutes);
-
 module.exports = router;
